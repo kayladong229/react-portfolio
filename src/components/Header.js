068771a0421ab05301142d084404fa56990ed20f
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-11-05 10:43:20 -0700

    Added separate Navigation component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 const styles = {
   headerStyle: {
     background: "#CAF0F8",
     display: 'flex',
     justifyContent: 'space-between',
-    height: '45px'
+    height: '42px'
   },
   headingStyle: {
     fontSize: "30",
   },
-  ulStyle: {
-    alignItems: 'center',
-  }
 };
 
 function Header({ currentPage, handlePageChange }) {
@@ -20,53 +18,7 @@ function Header({ currentPage, handlePageChange }) {
     <div>
       <header style={styles.headerStyle} className="header">
         <h1 style={styles.headingStyle}>Kayla Dong's Portfolio</h1>
-        <ul style={styles.ulStyle} className="nav nav-tabs">
-          <li className="nav-item">
-            <a
-              href="#about"
-              //If not currently on About Me page, we click the About tab to switch pages to the About Me page
-              onClick={() => handlePageChange("About")}
-              className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
-              }
-            >
-              About Me
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#portfolio"
-              onClick={() => handlePageChange("Portfolio")}
-              className={
-                currentPage === "Portfolio" ? "nav-link active" : "nav-link"
-              }
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#resume"
-              onClick={() => handlePageChange("Resume")}
-              className={
-                currentPage === "Resume" ? "nav-link active" : "nav-link"
-              }
-            >
-              Resume
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#contact"
-              onClick={() => handlePageChange("Contact")}
-              className={
-                currentPage === "Contact" ? "nav-link active" : "nav-link"
-              }
-            >
-              Contact Me
-            </a>
-          </li>
-        </ul>
+        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
       </header>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const styles = {
+  ulStyle: {
+    alignItems: "center",
+  },
+};
+
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <div>
+      <ul style={styles.ulStyle} className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            //If not currently on About Me page, we click the About tab to switch pages to the About Me page
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+            }
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link active" : "nav-link"
+            }
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact Me
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
