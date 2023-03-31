2b80969d80a93310e4081c88895f96fec3555fd0
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2023-03-31 09:58:23 -0700

    Updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -26,7 +26,7 @@ export default function Resume() {
       <p style={styles.text}>
         Click{" "}
         <a
-          href={require("../../assets/Kayla Dong Web Development _ Full Stack Resume.pdf")}
+          href={require("../../assets/Kayla Dong Resume 2023.pdf")}
           download
         >
           here
